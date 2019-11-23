0d9121ed56813e7f2b3fcc0699c5b7e925e0c497
kolpakre <kolpakre62@gmail.com>
2019-11-23 13:26:44 +0300

    Lab2

--- index.php
+++ index.php
@@ -1,27 +1,52 @@
 <?php
-class A {}
 
-class B extends A {
-	public $a;
-	public function __construct ($a) {
-		$this->a = $a;
+class Linear {
+	private $x;
+	public function llinear($a,$b){
+		if ($a == 0){
+			throw new Exception('Не делитьсяна 0');
+		} else {
+			MyLog::log("Это линейное уравнение");
+			$this->x = (-$b/$a);
+			return $this->x;
+		}
 	}
 }
 
-class C extends B {
-	public $b;
-	public function __construct ($a, $b, $c) {
-		$this->b = $b;
-		$this->c = $c;
-		parent::__construct($a)
+class Square extends Linear {
+	private $discr;
+
+	protected function discriminant($a,$b,$c){
+		return (($b*$b) - (4 * $a * $c));
+	}
+
+	public function solve($a,$b,$c)
+	{
+		if($a === 0)
+        {
+			return array($this->linear($b,$c));
+		}
+		else
+		{
+			$discr = $this->discriminant($a,$b,$c);
+			if ($discr < 0)
+			{
+				$this->x = false;
+				throw new Exception('Discriminant < 0');
+			}
+			elseif ($discr > 0)
+			{
+				MyLog::log("Это квадратное уравнение");
+				$t1=((-$b + sqrt($discr)) / (2 * $a));
+				$t2=((-$b - sqrt($discr)) / (2 * $a));
+				return $this->x= array($t1,$t2);
+			}
+		
+			else
+			{
+				return $this->x = array((-$b/2*$a));
+			}
+		}		
 	}
 }
-
-$test1 = new A();
-$test2 = new A();
-$test3 = new A();
-$test4 = new B($test2);
-$test5 = new C($test1, $test4, $test3);
-
-//var_dump($test5);
 ?>
